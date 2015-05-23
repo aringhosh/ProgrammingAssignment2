eed5b386a3cf74d2453f31c6eff2915006833c1b
Arin <arinv1618beta@gmail.com>
2015-05-23 17:44:00 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makecache_matrixMatrix <- function(mat_x = matrix()) 
+{
+        cached_matrix <- NULL
+        set <- function(mat_) 
+        {
+                mat_x <<- mat_
+                cached_matrix <<- NULL
+        }
+        get <- function() mat_x
+        set_ <- function(inv) cached_matrix <<- inv
+        inv_ <- function() cached_matrix
+        list(set = set, get = get, setMatrix = set_, getInverse = inv_)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cache_matrixSolve <- function(mat_x, ...) 
+{
+        cached_matrix <- mat_x$getInverse()
+        if (!is.null(cached_matrix)) 
+        {
+                return(cached_matrix)
+        }
+        else{
+            matrix <- mat_x$get()
+            tryCatch( 
+            {
+                cached_matrix <- solve(matrix, ...)
+            },
+            error = function(e_) 
+            {
+                message(c("Something is wrong here... here is the clue ",e_))
+                return(NA)
+            },
+            warning = function(w_) 
+            {
+                message(c("are you sure???? ",w_))
+                return(NA)
+            },
+            finally = 
+            {
+                mat_x$setMatrix(cached_matrix)
+            } )
+            return (cached_matrix)
+        } 
 }
