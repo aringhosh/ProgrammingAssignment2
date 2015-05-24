29605cd3704ed8395b759b3cdb4eafa2a37e5805
Arin <arinv1618beta@gmail.com>
2015-05-23 21:53:01 -0500

    Update cachematrix.R
    week 2 - asmnt 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,45 +1,41 @@
-makecache_matrixMatrix <- function(mat_x = matrix()) 
-{
-        cached_matrix <- NULL
-        set <- function(mat_) 
-        {
-                mat_x <<- mat_
-                cached_matrix <<- NULL
-        }
-        get <- function() mat_x
-        set_ <- function(inv) cached_matrix <<- inv
-        inv_ <- function() cached_matrix
-        list(set = set, get = get, setMatrix = set_, getInverse = inv_)
+# makeVector takes a numeric vector, saved in the private variable x
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
-cache_matrixSolve <- function(mat_x, ...) 
-{
-        cached_matrix <- mat_x$getInverse()
-        if (!is.null(cached_matrix)) 
-        {
-                return(cached_matrix)
-        }
-        else{
-            matrix <- mat_x$get()
-            tryCatch( 
-            {
-                cached_matrix <- solve(matrix, ...)
-            },
-            error = function(e_) 
-            {
-                message(c("Something is wrong here... here is the clue ",e_))
-                return(NA)
-            },
-            warning = function(w_) 
-            {
-                message(c("are you sure???? ",w_))
-                return(NA)
-            },
-            finally = 
-            {
-                mat_x$setMatrix(cached_matrix)
-            } )
-            return (cached_matrix)
-        } 
+# cachemean takes a caching Vector created with makeVector
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+#mean already exists i.e. cached
+    message("we have cached mean")
+    return(m)
+  }
+  
+  #mean is not cached
+  data <- x$get()
+  m <- mean(data, ...) #compute mean
+  x$setmean(m)
+  m #return m
 }
+
+## sample output
+
+#> x <- makeVector()
+#> x$set(matrix(1:8,4,2))
+#> cachemean(x)
+#[1] 4.5
+#> cachemean(x)
+#we have cached mean
+#[1] 4.5
